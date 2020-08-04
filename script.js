e953b3151c53b97b83c06bded20ff5638a360df8
Your Name <csr_4064@hotmail.com>
2020-08-03 22:27:17 -0300

    Iniciando el proyecto

---
+++ script.js
@@ -0,0 +1,27 @@
+// Cambiar el tema
+const elegirTema = document.getElementById('elegirTema');
+const tema = document.getElementById('tema');
+let clicks = 0;
+
+elegirTema.addEventListener('click', () =>{
+    
+    if(clicks % 2 == 0){
+        tema.style.visibility= 'visible';
+        //tema.style.zIndex = '10';
+    }
+    else{
+        tema.style.visibility= 'hidden';        
+    }
+    clicks++
+});
+
+// Cuando el menú está expandido
+
+const day = document.getElementById('day');
+const night = document.getElementById('night');
+const body = document.getElementById('cuerpo');
+
+night.addEventListener('click', () => {
+    body.style.backgroundColor = 'rgba(17,0,56,1)';
+})
+
